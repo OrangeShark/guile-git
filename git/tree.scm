223ecbc2e6aaf7a37119777ae9a50d3cf7e7b68d
Erik Edrosa <erik.edrosa@gmail.com>
2016-10-25 23:10:20 -0400

    Add git tree module

--- git/tree.scm
+++ git/tree.scm
@@ -25,6 +25,8 @@
               (tree->pointer source))
         (pointer->tree (dereference-pointer out))))))
 
+(define %tree-entry-free (dynamic-func "git_tree_entry_free" libgit2))
+
 (define tree-entry-byid
   (let ((proc (libgit2->procedure '* "git_tree_entry_byid" '(* *))))
     (lambda (tree id)
@@ -48,3 +50,45 @@
         (if (null-pointer? ret)
             #f
             (pointer->tree-entry ret))))))
+
+(define tree-entry-bypath
+  (let ((proc (libgit2->procedure* "git_tree_entry_bypath" '(* * *))))
+    (lambda (tree path)
+      (let ((out (make-double-pointer)))
+        (proc out (tree->pointer tree) (string->pointer path))
+        (pointer->tree-entry (pointer-gc (dereference-pointer out) %tree-entry-free))))))
+
+(define tree-entry-cmp
+  (let ((proc (libgit2->procedure int "git_tree_entry_cmp" '(* *))))
+    (lambda (e1 e2)
+      (proc (tree-entry->pointer e1) (tree-entry->pointer e2)))))
+
+(define tree-entry-bypath
+  (let ((proc (libgit2->procedure* "git_tree_entry_dup" '(* *))))
+    (lambda (source)
+      (let ((dest (make-double-pointer)))
+        (proc dest (tree-entry->pointer source))
+        (pointer->tree-entry (pointer-gc (dereference-pointer dest) %tree-entry-free))))))
+
+;; FIXME: https://libgit2.github.com/libgit2/#HEAD/group/tree/git_tree_entry_filemode
+
+;; FIXME: https://libgit2.github.com/libgit2/#HEAD/group/tree/git_tree_entry_filemode_raw
+
+(define tree-entry-id
+  (let ((proc (libgit2->procedure '* "git_tree_entry_id" '(*))))
+    (lambda (entry)
+      (let ((ret (proc (tree-entry->pointer entry))))
+        (pointer->oid ret)))))
+
+(define tree-entry-name
+  (let ((proc (libgit2->procedure '* "git_tree_entry_name" '(*))))
+    (lambda (entry)
+      (let ((ret (proc (tree-entry->pointer entry))))
+        (pointer->string ret)))))
+
+(define tree-entry->object
+  (let ((proc (libgit2->procedure* "git_tree_entry_to_object" '(* * *))))
+    (lambda (repository entry)
+      (let ((out (make-double-pointer)))
+        (proc out (repository->pointer repository) (tree-entry->pointer entry))
+        (pointer->object (dereference-pointer out))))))
