c1ebee0d15ac49e44faec47297f5bdea6313a407
Amirouche <amirouche@hypermove.net>
2016-10-31 15:57:34 +0100

    cosmit

--- git/annotated.scm
+++ git/annotated.scm
@@ -15,25 +15,25 @@
   (let ((proc (libgit2->procedure* "git_annotated_commit_from_fetchhead" '(* * * * *))))
     (lambda (repository branch-name remote-url id)
       (let ((out (make-double-pointer)))
-	(proc out
-	      (repository->pointer repository)
-	      (string->pointer branch-name)
-	      (string->pointer remote-url)
-	      (oid->pointer id))
-	(pointer->annotated-commit (dereference-pointer out))))))
+        (proc out
+              (repository->pointer repository)
+              (string->pointer branch-name)
+              (string->pointer remote-url)
+              (oid->pointer id))
+        (pointer->annotated-commit (dereference-pointer out))))))
 
 (define annotated-commit-from-ref
   (let ((proc (libgit2->procedure* "git_annotated_commit_from_ref" '(* * *))))
     (lambda (repository reference)
       (let ((out (make-double-pointer)))
-	(proc out (repository->pointer repository) (reference->pointer reference))
-	(pointer->annotated-commit (dereference-pointer out))))))
+        (proc out (repository->pointer repository) (reference->pointer reference))
+        (pointer->annotated-commit (dereference-pointer out))))))
 
 (define annotated-commit-from-revspec
   (let ((proc (libgit2->procedure* "git_annotated_commit_from_revspec" '(* * *))))
     (lambda (repository revspec)
       (let ((out (make-double-pointer)))
-	(proc out (repository->pointer repository) (string->pointer revspec))))))
+        (proc out (repository->pointer repository) (string->pointer revspec))))))
 
 (define annotated-commit-id
   (let ((proc (libgit2->procedure '* "git_annotated_commit_id" '(*))))
@@ -44,5 +44,5 @@
   (let ((proc (libgit2->procedure* "git_annotated_commit_lookup" '(* * *))))
     (lambda (repository id)
       (let ((out (make-double-pointer)))
-	(proc out (repository->pointer repository) (oid->pointer id))
-	(pointer->annotated-commit (dereference-pointer out))))))
+        (proc out (repository->pointer repository) (oid->pointer id))
+        (pointer->annotated-commit (dereference-pointer out))))))
