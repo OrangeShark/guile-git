c654ebccba5f9171e6d27ba82aa60ffe6aa9cf46
Amirouche <amirouche@hypermove.net>
2016-11-01 12:59:22 +0100

    Change license for 'GPL3+'

--- git.scm
+++ git.scm
@@ -1,3 +1,22 @@
+;;; Guile-Git --- GNU Guile bindings of libgit2
+;;; Copyright © 2016 Amirouche Boubekki <amirouche@hypermove.net>
+;;; Copyright © 2016 Erik Edrosa <erik.edrosa@gmail.com>
+;;;
+;;; This file is part of Guile-Git.
+;;;
+;;; Guile-Git is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Guile-Git is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Guile-Git.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (git))
 
 (eval-when (eval load compile)
--- git/bindings.scm
+++ git/bindings.scm
@@ -1,5 +1,22 @@
-;;; Copyright © Ludovic Courtès <ludo@gnu.org>
-;;; Released under the GNU GPL version 3 or later.
+;;; Guile-Git --- GNU Guile bindings of libgit2
+;;; Copyright © 2016 Amirouche Boubekki <amirouche@hypermove.net>
+;;; Copyright © 2016 Erik Edrosa <erik.edrosa@gmail.com>
+;;; Copyright © 2016 Ludovic Courtès <ludo@gnu.org>
+;;;
+;;; This file is part of Guile-Git.
+;;;
+;;; Guile-Git is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Guile-Git is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Guile-Git.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (git bindings)
   #:use-module (rnrs bytevectors)
