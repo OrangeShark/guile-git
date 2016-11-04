b94dd4fa2a670265d82bf62cd583d82a2ef04ffb
Erik Edrosa <erik.edrosa@gmail.com>
2016-11-03 23:32:34 -0400

    Remove git web app and git web routes module

--- git/web/app.scm
+++
@@ -1,8 +0,0 @@
-(define-module (git web app)
-  #:use-module (web server)
-  #:use-module (git web routes)
-  #:export (run-gitweb))
-
-
-(define (run-gitweb port)
-  (run-server (lambda args (apply route-handler args)) 'http `(#:port ,port)))
