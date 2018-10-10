b2619bf419d0d0614184f73a09ffdea4c2410418
Erik Edrosa <erik.edrosa@gmail.com>
2018-10-09 22:57:29 -0400

    tests: Add license and copyright notices to test files.
    * tests/branch.scm: Add license and copyright notice.
    * tests/commit.scm: Likewise.
    * tests/helpers.scm: Likewise.
    * tests/oid.scm: Likewise.
    * tests/reference.scm: Likewise.
    * tests/remote.scm: Likewise.
    * tests/repository.scm: Likewise.
    * tests/reset.scm: Likewise.
    * tests/rev-parse.scm: Likewise.
    * tests/status.scm: Likewise.
    * tests/tree.scm: Likewise.

--- tests/reference.scm
+++ tests/reference.scm
@@ -1,3 +1,23 @@
+;;; Guile-Git --- GNU Guile bindings of libgit2
+;;; Copyright © 2016 Amirouche Boubekki <amirouche@hypermove.net>
+;;; Copyright © 2017 Erik Edrosa <erik.edrosa@gmail.com>
+;;; Copyright © 2017 Ludovic Courtès <ludo@gnu.org>
+;;;
+;;; This file is part of Guile-Git.
+;;;
+;;; Guile-Git is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Guile-Git is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Guile-Git.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (tests reference)
   #:use-module (srfi srfi-64))
 
